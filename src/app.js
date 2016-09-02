da03797c117dd15bd8c01c30c3bdfab7ff24f3cf
lgh06 <liugehuan@kankan.com>
2016-09-02 15:24:49 +0800

    require-directory & app directory & routes

--- src/app.js
+++ src/app.js
@@ -5,14 +5,14 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
-
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'app/views'));
 app.set('view engine', 'hbs');
+
+//disable express header
+app.set('x-powered-by', false);
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -22,8 +22,13 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
+
+//mount routes with require-directory
+var requireDirectory = require('require-directory');
+var r = requireDirectory(module, './app/routes');
+
+var tools = require('./app/tools');
+tools.mount_routes(app,r);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
